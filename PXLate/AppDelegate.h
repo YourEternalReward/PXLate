b6a51f5b42cf440e48e2fbd683134dce22e9dc73
Mike <Mike@Mikes-Mac-Pro.local>
2013-10-28 06:25:11 -0700

    Initial Commit

---
+++ PXLate/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  PXLate
+//
+//  Created by Mike on 28/10/13.
+//  Copyright (c) 2013 Aon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
